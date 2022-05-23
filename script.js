a7a1d6c022826115e4d0a4a760137ecd0d34d23b
ariya <ariya_watchara@cmu.ac.th>
2022-05-23 14:49:42 +0700

    confirm

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function addStudentToTable(index,student){
     cell.appendChild(div)
     div.appendChild(image)
     image.setAttribute('src',student.image)
-    image.height = 200
+    image.style.height = '200px'
     image.classList.add('img-thumbnail')
     row.appendChild(cell)
     cell = document.createElement('td')
@@ -29,10 +29,26 @@ function addStudentToTable(index,student){
     cell = document.createElement('td')
     cell.innerHTML = student.description
     row.appendChild(cell)
+    cell = document.createElement('td')
+    let button = document.createElement('button')
+    button.classList.add('btn')
+    button.classList.add('btn-danger')
+    button.setAttribute('type', 'button')
+    button.innerText = 'delete'
+    button.addEventListener('click', function() {
+        let cf = `ท่านต้องการลบคุณ ${student.name} หรือไม่`;
+        if(confirm(cf)){
+            deleteStudent(student.id)
+        }
+    })
+    cell.appendChild(button)
+    row.appendChild(cell)
     tableBody.appendChild(row)
 }
 function addStudentList(studentlist){
     let counter = 1
+    const tableBody = document.getElementById('tableBody')
+    tableBody.innerHTML = ''
     for(student of studentlist){
         addStudentToTable(counter++ ,student)
     }
@@ -48,18 +64,96 @@ function addStudentData(student){
     gpaElem.innerHTML = student.gpa
     let profileElem = document.getElementById('image')
     profileElem.setAttribute('src', student.image)
+    image.style.height = '200px'
 }
-function onLoad(){
+function addStudentToDB(student){
+    fetch('https://dv-student-backend-2019.appspot.com/students',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(student)
+    }).then(response => {
+        if (response.status === 200){
+            return response.json()
+        }else{
+            throw Error(response.statusText)
+        }
+    }).then(data => {
+        console.log('success',data)
+        showAllStudents()
+    }).catch(error => {
+        return null
+    })
+}
+function deleteStudent(id){
+    fetch(`https://dv-student-backend-2019.appspot.com/student/${id}`,{
+        method: 'DELETE'
+    }).then(response => {
+        if (response.status === 200){
+            return response.json()
+        }else{
+            throw Error(response.statusText)
+        }
+    }).then(data => {
+        alert(`student name ${data.name} is now deleted`)
+        showAllStudents()
+    }).catch(error => {
+        alert('your input student id is not in database')
+    })
+}
+function onAddStudentClick(){
+    let student = {}
+    student.name = document.getElementById('nameInput').value
+    student.surname = document.getElementById('surnameInput').value
+    student.studentId = document.getElementById('studentIdInput').value
+    student.gpa = document.getElementById('gpaInput').value
+    student.image = document.getElementById('imageLinkInput').value
+    addStudentToDB(student)
+}
+document.getElementById('addButton').addEventListener('click',function() {
+    onAddStudentClick()
+})
+document.getElementById('searchButton').addEventListener('click',() =>{
+    let id = document.getElementById('inputText').value
+    console.log(id)
+    fetch(`https://dv-student-backend-2019.appspot.com/student/${id}`)
+    .then(response => {
+        return response.json()
+    }).then(student => {
+        addStudentData(student)
+    })
+})
+function showAllStudents(){
     fetch('https://dv-student-backend-2019.appspot.com/students').then(response => {
         return response.json()
     })
         .then(data =>{
             addStudentList(data)
         })
-    fetch('https://dv-student-backend-2019.appspot.com/student').then(response => {
+}
+// function onLoad(){
+//     student = {
+//         studentId: "642110332",
+//         name: "Ariya",
+//         surname: "Watchara-apanukorn",
+//         gpa: "6.78",
+//         image: "https://i.pinimg.com/474x/fe/5b/8c/fe5b8c05c2a4d1e50e3d0cf9925e1556.jpg"
+//     }
+//     addStudentToDB(student)
+//     fetch('https://dv-student-backend-2019.appspot.com/students').then(response => {
+//         return response.json()
+//     })
+//         .then(data =>{
+//             addStudentList(data)
+//         })
+// }
+function onLoad(){
+    // deleteStudent(27)
+    fetch('https://dv-student-backend-2019.appspot.com/students').then(response => {
         return response.json()
     })
         .then(data =>{
-            addStudentData(data)
+            addStudentList(data)
         })
 }
